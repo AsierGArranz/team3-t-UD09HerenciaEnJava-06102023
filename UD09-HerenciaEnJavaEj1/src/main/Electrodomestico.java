b959d8f7e83b1e351e1fafbb6d8152e1ea177f9a
adriii28 <adrigallego28@gmail.com>
2023-10-06 18:42:52 +0200

    Metodos electrodomesticos

--- UD09-HerenciaEnJavaEj1/src/main/Electrodomestico.java
+++ UD09-HerenciaEnJavaEj1/src/main/Electrodomestico.java
@@ -2,40 +2,124 @@ package main;
 
 public class Electrodomestico {
 	
-	protected double precioBase;
-	protected String color;
-	protected char consumoEnergetico;
-	protected double peso;
-	
+
 	final int PRECIO_BASE = 100;
 	final String COLOR_BASE = "Blanco";
 	final char CONSUMO_BASE = 'F';
 	final double PESO_BASE = 5;
 	
+	protected double precioBase = PRECIO_BASE;
+	protected String color = COLOR_BASE;
+	protected char consumoEnergetico = CONSUMO_BASE;
+	protected double peso = PESO_BASE;
 	
 	public Electrodomestico() {
-		this.precioBase = PRECIO_BASE;
-		this.color = COLOR_BASE;
-		this.consumoEnergetico = CONSUMO_BASE;
-		this.peso = PESO_BASE;
+	
 	}
 
 	public Electrodomestico(double precioBase, double peso) {
 		this.precioBase = precioBase;
-		this.color = COLOR_BASE;
-		this.consumoEnergetico = CONSUMO_BASE;
 		this.peso = peso;
 	}
 
 	public Electrodomestico(double precioBase, String color, char consumoEnergetico, double peso) {
+		
+		comprobarConsumoEnergetico(consumoEnergetico);
+
 		this.precioBase = precioBase;
 		this.color = color;
-		this.consumoEnergetico = consumoEnergetico;
 		this.peso = peso;
+		
 	}
 	
+	public void comprobarConsumoEnergetico(char letra) {
+		if (letra == 'A' || letra == 'B' || letra == 'C' || letra == 'D' || letra == 'E' || letra == 'F') {
+			this.consumoEnergetico = letra;
+		} else {
+			letra = CONSUMO_BASE;
+		}
+	}
 	
+	public void comprobarColor(String color) {
+		
+	}
 	
+	public void precioFinal() {
+		switch (consumoEnergetico) {
+		case 'A':
+			precioBase+=100;
+			
+			break;
+		case 'B':
+			precioBase+=80;
+
+			break;
+		case 'C':
+			precioBase+=60;
+
+			break;
+		case 'D':
+			precioBase+=50;
+
+			break;
+		case 'E':
+			precioBase+=30;
+
+			break;
+		case 'F':
+			precioBase+=10;
+
+			break;
+
+		default:
+			break;
+		}
+		
+		if (0<=peso&&peso<=19) {
+			precioBase+=10;
+
+		} else if (20<=peso&&peso<=49) {
+			precioBase+=50;
+
+		} else if (50<=peso&&peso<=79) {
+			precioBase+=80;
+
+		} else if (peso>=80) {
+			precioBase+=100;
+		}	
+	}
+
+	public double getPrecioBase() {
+		return precioBase;
+	}
+
+	public void setPrecioBase(double precioBase) {
+		this.precioBase = precioBase;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public char getConsumoEnergetico() {
+		return consumoEnergetico;
+	}
+
+	public void setConsumoEnergetico(char consumoEnergetico) {
+		this.consumoEnergetico = consumoEnergetico;
+	}
+
+	public double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}	
 	
 
 }
