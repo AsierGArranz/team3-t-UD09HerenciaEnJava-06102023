eb6a6d83b939abf35677ed8d885cf022612b2c31
adriii28 <adrigallego28@gmail.com>
2023-10-06 17:36:13 +0200

    Crear libros

--- UD09-HerenciaEnJavaEj3/src/main/mainApp.java
+++ UD09-HerenciaEnJavaEj3/src/main/mainApp.java
@@ -3,7 +3,12 @@ package main;
 public class mainApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Libro libro1 = new Libro("12345","Libro programacion","Stephen Hawkings",150);
+		System.out.println(libro1);
+		
+		Libro libro2 = new Libro("54321","Manolito Gafotas","Elvira Lindo",192);
+		System.out.println(libro2);
+
 
 	}
 
