e7b249c142346508ee0170074dafa8b340cd186f
AsierGArranz <asiergarranz@gmail.com>
2023-10-09 17:22:18 +0200

    alumnos y profesores

--- UD09-HerenciaEnJavaEj5/src/main/Alumno.java
+++ UD09-HerenciaEnJavaEj5/src/main/Alumno.java
@@ -2,10 +2,29 @@ package main;
 
 public class Alumno extends Persona{
 
+	private int nota = 0;
+	
+	
+	public int getNota() {
+		return nota;
+	}
+
+
+	public void setNota(int nota) {
+		this.nota = nota;
+	}
+
+
 	@Override
 	public boolean asistencia() {
-		// TODO Auto-generated method stub
-		return false;
+		// 50%
+		int random = (int)(Math.random()*2+1);
+		if(random ==1) {
+			return true;
+		}else{
+			return false;
+		}
 	}
+	//comentario
 
 }
--- UD09-HerenciaEnJavaEj5/src/main/Profesor.java
+++ UD09-HerenciaEnJavaEj5/src/main/Profesor.java
@@ -1,11 +1,30 @@
 package main;
 
 public class Profesor extends Persona{
+	
+	private String materia = "";
+	
+
+	public String getMateria() {
+		return materia;
+	}
+
+
+	public void setMateria(String materia) {
+		this.materia = materia;
+	}
+
 
 	@Override
 	public boolean asistencia() {
-		// TODO Auto-generated method stub
-		return false;
+		// 20%
+		int random = (int)(Math.random()*5+1);
+		if(random ==1) {
+			return false;
+		}else{
+			return true;
+		}
 	}
+	//comentario
 
 }
