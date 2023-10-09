b82eacca635d5ab6373ca357aa8ec70823390030
AsierGArranz <asiergarranz@gmail.com>
2023-10-09 17:20:53 +0200

    alumnos y profesores

--- UD09-HerenciaEnJavaEj5/src/main/Alumno.java
+++ UD09-HerenciaEnJavaEj5/src/main/Alumno.java
@@ -2,4 +2,10 @@ package main;
 
 public class Alumno extends Persona{
 
+	@Override
+	public boolean asistencia() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 }
--- UD09-HerenciaEnJavaEj5/src/main/Profesor.java
+++ UD09-HerenciaEnJavaEj5/src/main/Profesor.java
@@ -2,4 +2,10 @@ package main;
 
 public class Profesor extends Persona{
 
+	@Override
+	public boolean asistencia() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 }
