5c0b70df4fd73a7cd3d36b49f233e8b4458396a8
AsierGArranz <asiergarranz@gmail.com>
2023-10-09 16:21:31 +0200

    ejercicio4 primera mitad

--- UD09-HerenciaEnJavaEj4/src/main/Raices.java
+++ UD09-HerenciaEnJavaEj4/src/main/Raices.java
@@ -1,5 +1,27 @@
 package main;
 
 public class Raices {
-
+	
+	private double a = 0;
+	private double b = 0;
+	private double c = 0;
+	
+	public Raices(int a, int b, int c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+	
+	public double getDiscriminante() {
+		return (Math.pow(b,2))-4*a*c;
+	}
+	
+	public boolean tieneRaices() {
+		return getDiscriminante()>=0;
+	}
+	
+	public boolean tieneRaiz() {
+		return getDiscriminante()==0;
+	}
+	
 }
